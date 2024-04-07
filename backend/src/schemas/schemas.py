adf1bbc7239879547fafba22daa4b791c855eae0
Kagami <shimura.kyohei7617@gmail.com>
2024-04-07 18:04:28 +0900

    feat: genre schema

--- backend/src/schemas/schemas.py
+++ backend/src/schemas/schemas.py
@@ -1,23 +1,22 @@
-from dataclasses import Field
 from datetime import datetime
 
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 
 
 class GenreCreate(BaseModel):
-    main_genre_name: str = Field(gt=0, max_length=255, examples=["プログラミング"])
-    sub_genre_name: str = Field(gt=0, max_length=255, examples=["Python"])
+    main_genre_name: str = Field(max_length=255, examples=["プログラミング"])
+    sub_genre_name: str = Field(max_length=255, examples=["Python"])
 
 
 class GenereUpdate(BaseModel):
-    main_genre_name: str = Field(gt=0, max_length=255, examples=["プログラミング"])
-    sub_genre_name: str = Field(gt=0, max_length=255, examples=["Python"])
+    main_genre_name: str = Field(max_length=255, examples=["プログラミング"])
+    sub_genre_name: str = Field(max_length=255, examples=["Python"])
 
 
 class GenereResponse(BaseModel):
     genre_id: int = Field(gt=0, examples=[1])
-    main_genre_name: str = Field(gt=0, max_length=255, examples=["プログラミング"])
-    sub_genre_name: str = Field(gt=0, max_length=255, examples=["Python"])
+    main_genre_name: str = Field(max_length=255, examples=["プログラミング"])
+    sub_genre_name: str = Field(max_length=255, examples=["Python"])
     created_at: datetime
     updated_at: datetime
 
