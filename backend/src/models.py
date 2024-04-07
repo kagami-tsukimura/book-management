7279eb76ea52767b3114c977c142d2c630ee436d
Kagami <shimura.kyohei7617@gmail.com>
2024-04-07 16:45:16 +0900

    feat: genre model

---
+++ backend/src/models.py
@@ -0,0 +1,15 @@
+from database.database import Base
+from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy.sql.functions import current_timestamp
+
+
+class Genre(Base):
+    __tablename__ = "genre_master"
+
+    genre_id = Column(Integer, primary_key=True)
+    main_genre_name = Column(String, unique=True, nullable=False)
+    sub_genre_name = Column(String, unique=True, nullable=False)
+    created_at = Column(DateTime, default=current_timestamp())
+    updated_at = Column(
+        DateTime, default=current_timestamp(), onupdate=current_timestamp()
+    )
