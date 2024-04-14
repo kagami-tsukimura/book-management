65bc98c84741d82adc95fb90c6e5ef7e95f85e70
Kagami <shimura.kyohei7617@gmail.com>
2024-04-14 17:40:10 +0900

    auto: auto command push

---
+++ backend/src/tests/conftest.py
@@ -0,0 +1,37 @@
+# flake8: noqa: E402
+import os
+import sys
+
+current_dir = os.path.join(os.path.dirname(__file__), "../")
+sys.path.append(current_dir)
+
+import pytest
+from models import Base, Genre
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.pool import StaticPool
+
+
+@pytest.fixture()
+def session_fixture():
+    """
+    url: sqliteを指定してメモリ上で動作
+    """
+    engine = create_engine(
+        url="sqlite://", connect_args={"check_same_thread": False}, poolclass=StaticPool
+    )
+    # modelsで定義したテーブルをDBに作成
+    Base.metadata.create_all(engine)
+
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    db = SessionLocal()
+
+    try:
+        genre1 = Genre(main_genre_name="IT", sub_genre_name="Python")
+        genre2 = Genre(main_genre_name="IT", sub_genre_name="Rust")
+        db.add(genre1)
+        db.add(genre2)
+        db.commit()
+        yield db
+    finally:
+        db.close()
