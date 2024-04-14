a1b41ac79a8d0236362e4b372feefd75a982b667
Kagami <shimura.kyohei7617@gmail.com>
2024-04-14 17:46:30 +0900

    auto: auto command push

--- backend/src/tests/conftest.py
+++ backend/src/tests/conftest.py
@@ -6,6 +6,9 @@ current_dir = os.path.join(os.path.dirname(__file__), "../")
 sys.path.append(current_dir)
 
 import pytest
+from database.database import get_db
+from fastapi.testclient import TestClient
+from main import app
 from models import Base, Genre
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
@@ -35,3 +38,20 @@ def session_fixture():
         yield db
     finally:
         db.close()
+
+
+@pytest.fixture()
+# fixtureで別のfixtureを呼び出せる
+def client_fixture(session_fixture: Session):
+    def override_get_db():
+        return session_fixture
+
+    # appオブジェクトで使用するget_dbを今回のsqliteで上書き
+    app.dependency_overrides[get_db] = override_get_db
+    app.dependency_overrides[get_db] = override_get_db
+
+    client = TestClient(app)
+    yield client
+
+    # 上書きしたDIを戻す
+    app.dependency_overrides.clear()
