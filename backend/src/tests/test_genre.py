f0b4a38ea61b2577bee4e711e6a047dbd4862080
Kagami <shimura.kyohei7617@gmail.com>
2024-04-22 22:25:13 +0900

    auto: auto command push

--- backend/src/tests/test_genre.py
+++ backend/src/tests/test_genre.py
@@ -8,13 +8,18 @@ def test_find_all(client_fixture: TestClient):
     assert len(genres) == 2
 
 
-def test_find_by_id(client_fixture: TestClient):
+def test_find_by_id_success(client_fixture: TestClient):
     response = client_fixture.get("/genre/1")
     assert response.status_code == 200
     genre = response.json()
     assert genre["genre_id"] == 1
     assert genre["main_genre_name"] == "IT"
     assert genre["sub_genre_name"] == "Python"
+
+
+def test_find_by_id_failure(client_fixture: TestClient):
+    response = client_fixture.get("/genre/10")
+    assert response.status_code == 404
 
 
 def test_find_by_name(client_fixture: TestClient):
@@ -32,3 +37,6 @@ def test_create(client_fixture: TestClient):
     genre = response.json()
     assert genre["main_genre_name"] == "教養"
     assert genre["sub_genre_name"] == "簿記"
+
+
+# def test_update_success(client_fixture: TestClient):
