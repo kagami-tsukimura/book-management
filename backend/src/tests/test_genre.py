bfba4eababca6ca3e181b141ed048ac40ac70091
Kagami <shimura.kyohei7617@gmail.com>
2024-04-22 22:22:40 +0900

    feat: test genre create

--- backend/src/tests/test_genre.py
+++ backend/src/tests/test_genre.py
@@ -22,3 +22,13 @@ def test_find_by_name(client_fixture: TestClient):
     assert response.status_code == 200
     genres = response.json()
     assert len(genres) == 2
+
+
+def test_create(client_fixture: TestClient):
+    response = client_fixture.post(
+        "/genre", json={"main_genre_name": "教養", "sub_genre_name": "簿記"}
+    )
+    assert response.status_code == 201
+    genre = response.json()
+    assert genre["main_genre_name"] == "教養"
+    assert genre["sub_genre_name"] == "簿記"
