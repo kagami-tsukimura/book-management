c3d8fa1f806fa3557eb9c3dd13138a944d33139b
Kagami <shimura.kyohei7617@gmail.com>
2024-04-23 22:11:29 +0900

    feat: slice dir

--- backend/src/tests/conftest.py
+++ backend/src/tests/conftest.py
@@ -6,20 +6,21 @@ current_dir = os.path.join(os.path.dirname(__file__), "../")
 sys.path.append(current_dir)
 
 import pytest
-from database.database import get_db
-from fastapi.testclient import TestClient
-from main import app
 from models import Base, Genre
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import StaticPool
 
 
 @pytest.fixture()
-def session_fixture():
+def session():
     """
-    url: sqliteを指定してメモリ上で動作
+    テストのためのセッションオブジェクトを作成するfixtureです。
+
+    戻り値:
+        Session: データベースとのやりとりに使用するセッションオブジェクトです。
     """
+
     engine = create_engine(
         url="sqlite://", connect_args={"check_same_thread": False}, poolclass=StaticPool
     )
@@ -27,31 +28,24 @@ def session_fixture():
     Base.metadata.create_all(engine)
 
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-    db = SessionLocal()
 
-    try:
-        genre1 = Genre(main_genre_name="IT", sub_genre_name="Python")
-        genre2 = Genre(main_genre_name="IT", sub_genre_name="Rust")
-        db.add(genre1)
-        db.add(genre2)
-        db.commit()
-        yield db
-    finally:
-        db.close()
+    return SessionLocal()
 
 
-@pytest.fixture()
-# fixtureで別のfixtureを呼び出せる
-def client_fixture(session_fixture: Session):
-    def override_get_db():
-        return session_fixture
+# @pytest.fixture()
+# def session_fixture(session: pytest.Session):
+#     """
+#     url: sqliteを指定してメモリ上で動作
+#     """
 
-    # appオブジェクトで使用するget_dbを今回のsqliteで上書き
-    app.dependency_overrides[get_db] = override_get_db
-    app.dependency_overrides[get_db] = override_get_db
+#     db = session
 
-    client = TestClient(app)
-    yield client
-
-    # 上書きしたDIを戻す
-    app.dependency_overrides.clear()
+#     try:
+#         genre1 = Genre(main_genre_name="IT", sub_genre_name="Python")
+#         genre2 = Genre(main_genre_name="IT", sub_genre_name="Rust")
+#         db.add(genre1)
+#         db.add(genre2)
+#         db.commit()
+#         yield db
+#     finally:
+#         db.close()
